acf694139ea540186a1c45566012d52926ca77fd
Tom White <tom@cloudera.com>
2015-04-20 10:33:04 +0100

    Use a Kite Partitioner.

--- adam-cli/src/main/scala/org/bdgenomics/adam/cli/Partition.scala
+++ adam-cli/src/main/scala/org/bdgenomics/adam/cli/Partition.scala
@@ -17,21 +17,23 @@
  */
 package org.bdgenomics.adam.cli
 
-import java.io.File
+import java.io.{ ObjectInputStream, ObjectOutputStream, IOException, File }
 
 import org.apache.avro.Schema
 import org.apache.avro.generic.IndexedRecord
 import org.apache.hadoop.mapreduce.Job
-import org.apache.spark.{ Logging, SparkContext }
+import org.apache.spark._
 import org.apache.spark.rdd.MetricsContext._
-import org.bdgenomics.adam.rdd.ADAMContext._
 import org.bdgenomics.adam.util.HadoopUtil
 import org.bdgenomics.utils.instrumentation.Metrics
 import org.kitesdk.data.mapreduce.DatasetKeyOutputFormat
-import org.kitesdk.data.{ DatasetDescriptor, Datasets, Formats }
+import org.kitesdk.data.spi.{ StorageKey, DataModelUtil, EntityAccessor, PartitionStrategyParser }
+import org.kitesdk.data.{ PartitionStrategy, DatasetDescriptor, Datasets, Formats }
 import org.kohsuke.args4j.{ Option => Args4jOption, Argument }
 import parquet.avro.AvroParquetInputFormat
 import parquet.hadoop.util.ContextUtil
+
+import scala.util.control.NonFatal
 
 object Partition extends ADAMCommandCompanion {
   val commandName = "partition"
@@ -42,7 +44,7 @@ object Partition extends ADAMCommandCompanion {
   }
 }
 
-class PartitionArgs extends Args4jBase with ParquetSaveArgs {
+class PartitionArgs extends Args4jBase with ParquetSaveArgs with Serializable {
   @Args4jOption(required = true, name = "-partition_strategy_file",
     usage = "A JSON file containing the partition strategy")
   var partitionStrategyFile: File = _
@@ -55,7 +57,7 @@ class PartitionArgs extends Args4jBase with ParquetSaveArgs {
 }
 
 class Partition(val args: PartitionArgs) extends ADAMSparkCommand[PartitionArgs]
-    with Logging {
+    with Logging with Serializable {
   val companion = Partition
 
   def run(sc: SparkContext, job: Job) {
@@ -80,8 +82,59 @@ class Partition(val args: PartitionArgs) extends ADAMSparkCommand[PartitionArgs]
     val dataset: org.kitesdk.data.View[IndexedRecord] =
       Datasets.create("dataset:" + args.outputPath, desc, classOf[IndexedRecord])
     DatasetKeyOutputFormat.configure(job).writeTo(dataset)
-
     records.map(r => r.swap)
+      .partitionBy(new KitePartitioner(sc.defaultParallelism, dataset.getType, schema,
+        desc.getPartitionStrategy))
       .saveAsNewAPIHadoopDataset(job.getConfiguration)
+  }
+
+}
+
+class KitePartitioner(partitions: Int,
+                      datasetType: Class[IndexedRecord],
+                      @transient var schema: Schema,
+                      @transient var partitionStrategy: PartitionStrategy) extends Partitioner {
+
+  @transient var accessor: EntityAccessor[IndexedRecord] =
+    DataModelUtil.accessor(datasetType, schema)
+  @transient var key: StorageKey = new StorageKey(partitionStrategy)
+
+  override def numPartitions: Int = partitions
+
+  override def getPartition(k: Any): Int = {
+    val record = k.asInstanceOf[IndexedRecord]
+    accessor.keyFor(record, null, key)
+    nonNegativeMod(key.hashCode(), numPartitions)
+  }
+
+  def nonNegativeMod(x: Int, mod: Int): Int = {
+    val rawMod = x % mod
+    rawMod + (if (rawMod < 0) mod else 0)
+  }
+
+  @throws(classOf[IOException])
+  private def writeObject(out: ObjectOutputStream): Unit = {
+    try {
+      out.defaultWriteObject()
+      out.writeUTF(schema.toString)
+      out.writeUTF(partitionStrategy.toString)
+    } catch {
+      case e: IOException => throw e
+      case NonFatal(t)    => throw new IOException(t)
+    }
+  }
+
+  @throws(classOf[IOException])
+  private def readObject(in: ObjectInputStream): Unit = {
+    try {
+      in.defaultReadObject()
+      schema = Schema.parse(in.readUTF())
+      partitionStrategy = PartitionStrategyParser.parse(in.readUTF())
+      accessor = DataModelUtil.accessor(datasetType, schema)
+      key = new StorageKey(partitionStrategy)
+    } catch {
+      case e: IOException => throw e
+      case NonFatal(t)    => throw new IOException(t)
+    }
   }
 }
